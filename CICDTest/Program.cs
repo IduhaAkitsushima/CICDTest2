a1bcde2fbb7ef372e33d27b5906903e7ec865507
katsuta <amraamnit09@gmail.com>
2025-08-12 17:56:17 +0900

    First commit

--- CICDTest/Program.cs
+++ CICDTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestLib;
 
 namespace CICDTest
 {
@@ -10,6 +11,11 @@ namespace CICDTest
     {
         private static void Main(string[] args)
         {
+            Class1 class1 = new Class1();
+            int result = class1.Add(2, 3);
+            Console.WriteLine($"The result of adding 2 and 3 is: {result}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
---
+++ TestLib.Test/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using TestLib;
+
+namespace TestLib.Test
+{
+    public class Class1Tests
+    {
+        [Fact]
+        public void Add_ReturnsSum_WhenGivenTwoIntegers()
+        {
+            // Arrange
+            var class1 = new Class1();
+
+            // Act
+            int result = class1.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
